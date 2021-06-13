7228234c413a87a3ac9668b3422a0bbefa02b6e4
Timmy Nguyen <timmy-thai-nguyen@lambdastudents.com>
2021-06-13 01:52:34 -0700

    saving to canvas, Login form almost built out

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,61 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
+import axios from 'axios';
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-
-  const error = "";
+  // [x] 1. Build a form containing a username and password field.
+  const [formValues, setFormValues]= useState({
+    username:'',
+    password:''
+  }); 
+  // [x] 2. Add whatever state nessiary for form functioning.
   //replace with error state
+  const [error, setError] = useState('username and/or password not valid');
+  
+  const handleChanges = (e) =>{
+    setFormValues({...formValues, [e.target.name]:e.target.value})
+  };
 
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    // will be adding an axios call...
+    axios
+      .post('http://localhost:3000') // placeholder 'http://localhost:3000'
+  }
+// [x] 3. MAKE SURE YOUR USERNAME AND PASSWORD INPUTS INCLUDE data-testid="username" and data-testid="password"
+//4. If either the username or password is not entered, display the following words with the p tag provided: Username or Password not valid.
+//5. If the username / password is equal to Lambda / i<3Lambd4, save that token to localStorage.
   return (
     <div>
       <h1>Welcome to the Bubble App!</h1>
       <div data-testid="loginForm" className="login-form">
-        <h2>Build login form here</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            data-testid='username'
+            name='username'
+            type='text'
+            value={formValues.username}
+            placeholder='username'
+            onChange={handleChanges}
+          />
+          <input 
+            data-testid='password'
+            name='password'
+            type='text'
+            value={formValues.password}
+            placeholder='password'
+            onChange={handleChanges}
+          />
+
+          <button>Login</button>
+
+          {formValues.username ==='Lambda' && formValues.password === 'i<3Lambd4' ? null : <p data-testid="errorMessage" className="error">{error}</p>}
+        </form>
       </div>
 
-      <p data-testid="errorMessage" className="error">{error}</p>
     </div>
   );
 };
 
 export default Login;
-
-//Task List:
-//1. Build a form containing a username and password field.
-//2. Add whatever state nessiary for form functioning.
-//3. MAKE SURE YOUR USERNAME AND PASSWORD INPUTS INCLUDE data-testid="username" and data-testid="password"
-//4. If either the username or password is not entered, display the following words with the p tag provided: Username or Password not valid.
-//5. If the username / password is equal to Lambda / i<3Lambd4, save that token to localStorage.

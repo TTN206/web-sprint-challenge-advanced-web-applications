bc406b2f7db75b313db54fa767e6bed142d38855
Timmy Nguyen <timmy-thai-nguyen@lambdastudents.com>
2021-06-13 03:10:22 -0700

    app logout done, privateroute of bubblepage linked

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+// importing components and other nessesary pieces
+import {axiosWithAuth} from './helpers/axiosWithAuth';
+import PrivateRoute from './components/PrivateRoute';
+import BubblePage from './components/BubblePage';
 
 function App() {
+
+  const logout = () =>{
+    localStorage.removeItem('token');
+    window.location.href='/';
+  }
+
   return (
     <Router>
       <div className="App">
         <header>
           Color Picker Sprint Challenge
-          <a data-testid="logoutButton" href="#">logout</a>
+          <a data-testid="logoutButton" href="/" onClick={logout} >logout</a>
         </header> 
 
+      <Switch>
+        <PrivateRoute path='/bubbles' component={BubblePage} />
+
         <Route exact path="/" component={Login} />
+      </Switch>  
       </div>
     </Router>
   );
@@ -22,5 +36,5 @@ function App() {
 export default App;
 
 //Task List:
-//1. Render BubblePage as a PrivateRoute
-//2. Build the logout button to remove the localStorage Item.
+// [x] 1. Render BubblePage as a PrivateRoute
+// [ ] 2. Build the logout button to remove the localStorage Item.
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
-import axios from 'axios';
+// import axios from 'axios';
+import {useHistory} from 'react-router-dom';
+import {axiosWithAuth} from '../helpers/axiosWithAuth';
+
 
 const Login = () => {
   // make a post request to retrieve a token from the api
@@ -12,6 +15,8 @@ const Login = () => {
   // [x] 2. Add whatever state nessiary for form functioning.
   //replace with error state
   const [error, setError] = useState('username and/or password not valid');
+
+  const history = useHistory();
   
   const handleChanges = (e) =>{
     setFormValues({...formValues, [e.target.name]:e.target.value})
@@ -20,12 +25,22 @@ const Login = () => {
   const handleSubmit = (e) =>{
     e.preventDefault();
     // will be adding an axios call...
-    axios
-      .post('http://localhost:3000') // placeholder 'http://localhost:3000'
+    // axios
+    //   .post('http://localhost:3000') // placeholder 'http://localhost:3000'
+    axiosWithAuth()
+      .post('/login', formValues)
+      .then((res)=>{
+        console.log(res)
+        localStorage.setItem('token', res.data.payload)
+        history.push('/bubbles')
+      })
+      .catch((err)=>{
+        console.log(err.response, 'Houston, we got problems')
+      })
   }
 // [x] 3. MAKE SURE YOUR USERNAME AND PASSWORD INPUTS INCLUDE data-testid="username" and data-testid="password"
-//4. If either the username or password is not entered, display the following words with the p tag provided: Username or Password not valid.
-//5. If the username / password is equal to Lambda / i<3Lambd4, save that token to localStorage.
+// [x] 4. If either the username or password is not entered, display the following words with the p tag provided: Username or Password not valid.
+// [x] 5. If the username / password is equal to Lambda / i<3Lambd4, save that token to localStorage.
   return (
     <div>
       <h1>Welcome to the Bubble App!</h1>
